b1fa5a81d9ea99f08abb243b771a76790fe78131
fixalot <fixalot84@gmail.com>
2020-02-06 11:05:52 +0900

    initialize
    Signed-off-by: fixalot <fixalot84@gmail.com>

---
+++ README.md
@@ -0,0 +1,56 @@
+# python-lab
+
+python lab.
+
+#### environments
+
+- Python 3.8.1
+
+## 파이썬 설치
+
+[다운로드 링크](https://www.python.org/downloads/)
+
+## 특정 모듈 설치
+
+pip나 easy_install 실행파일이 있는 경로로 이동 후
+
+```py
+> pip install 모듈명
+```
+
+```py
+> easy_install 모듈명
+```
+
+pip는 윈도우10 + 기본 설치일 경우 `C:\Users\윈도우유저명\AppData\Local\Programs\Python\Python38-32\Scripts` 요딴 경로에 있다.
+
+가령 BeautifulSoup4을 사용하려면 일단 모듈 설치:
+
+```py
+> pip install beautifulsoup4
+```
+
+이 후 다음처럼 프로그램 파일을 작성한다:
+
+```py
+from bs4 import BeautifulSoup
+
+html_doc = '<div></div>'
+# print(html_doc)
+
+soup = BeautifulSoup(html_doc, 'html.parser')
+```
+
+## 프로그램 파일 작성
+
+파이썬 설치 시 딸려오는 IDLE 편집기를 사용하거나 직접 프로그램 파일을 작성한다. 파이썬 파일의 확장명은 보통 `py`로 한다.
+
+## 프로그램 파일 실행
+
+파일이 있는 경로에서 `py`로 실행한다:
+
+```py
+> py test.py
+```
+
+IDLE 편집기로 파일을 열고 있는 상태라면 `f5`로 즉시 실행할 수 있다.
---
+++ bs.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+
+html_doc = '<a class="item-subject" href="https://123.com/bbs/board.php?bo_table=aboard01&amp;wr_id=5452"> <span class="orangered visible-xs pull-right wr-comment"> <i class="fa fa-comment lightgray"></i> <b>+41</b>    #댓글수 </span> <span class="wr-icon wr-new"></span> #새글을 알리는 아이콘           오늘의 날씨 이유        # 제목 <b class="count orangered hidden-xs">+41</b>    #댓글수 </a>, <a class="item-subject" href="https://123.com/bbs/board.php?bo_table=aboard01&amp;wr_id=5451"> <span class="orangered visible-xs pull-right wr-comment"> <i class="fa fa-comment lightgray"></i> <b>+19</b> </span> <span class="wr-icon wr-new"></span>                            날씨 너무춥네요 ㅠ <b class="count orangered hidden-xs">+19</b> </a>, <a class="item-subject" href="https://123.com/bbs/board.php?bo_table=aboard01&amp;wr_id=5454"> <span class="orangered visible-xs pull-right wr-comment"> <i class="fa fa-comment lightgray"></i> <b>+23</b> </span> <span class="wr-icon wr-new"></span>                            우한 코로나 <b class="count orangered hidden-xs">+23</b> </a>'
+# print(html_doc)
+
+soup = BeautifulSoup(html_doc, 'html.parser')
+
+subjects = soup.select('.item-subject')  #게시글 클래스 선택
+for subject in subjects:
+##    print(subject.select_one('.item-subject').text)
+    print(subject.text)
+
+##print(soup.get_text())
+
