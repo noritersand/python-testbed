3b66a8c31319ce17b7a52b4b67cc384d8953802e
fixalot <fixalot84@gmail.com>
2020-04-08 10:31:40 +0900

    명판사 초롱아재 등장

--- discord/judge-bot.py
+++ discord/judge-bot.py
@@ -6,7 +6,7 @@ client = discord.Client()
 async def on_ready():
     print(client.user.id)
     print("ready")
-    game = discord.Game("상태메시지")
+    game = discord.Game("법규를 준수하는지 감시")
     await client.change_presence(status=discord.Status.online, activity=game)
 
 @client.event
@@ -14,4 +14,5 @@ async def on_message(message):
     if message.content.startswith("!안녕"):
         await message.channel.send("안녕하세요")
 
-client.run("Njk2OTg5ODQ2ODgzMDA4NTcy.Xoww_A.G5UHiidgGboVNlID4K_zy1rgpsY")
+client.run("Njk3MjUyNjA0NTg1MTgxMjQ0.Xo0lYw.007BhwgNGc5-_6f44P8EtXgkIho")
+
