9238d62cb2d8b574a8d3697ac7d8b51f1251bc4b
fixalot84 <fixalot@outlook.kr>
2022-06-14 09:36:06 +0900

    Update

--- worker/replace-file-name.py
+++ worker/replace-file-name.py
@@ -1,16 +1,22 @@
 import os
 
-folder = "c:/dev/repo/python-lab/hashcode/"
+folder = "c:/dev/temp/"
 
 for fileName in os.listdir(folder):
-    if "hashcode" not in fileName:
-        continue
-
     source = folder + fileName
-    # print(source)
+    print(source)
+
+    # 파일명에서 hashcode 제거하기
+    # if "hashcode" not in fileName:
+        # continue
     # print(fileName[8:len(fileName)])
-    destination = folder + fileName[8 : len(fileName)]
+    # destination = folder + fileName[8 : len(fileName)]
+    # os.rename(source, destination)
 
+    # 언더바를 하이픈으로
+    newFileName = fileName.replace('_', '-')
+    print(newFileName)
+    destination = folder + newFileName
     os.rename(source, destination)
-
-print(os.listdir(folder))
+    
+# print(os.listdir(folder))
