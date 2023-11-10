856a713fcf460e586e8e1d91740251d1d1b76f62
fixalot84 <fixalot@outlook.kr>
2023-11-10 11:43:18 +0900

    Update

--- worker/image-downloader/image_downloader.py
+++ worker/image-downloader/image_downloader.py
@@ -2,22 +2,11 @@ import os
 import requests  # request img from web
 import shutil  # save img locally
 
+from target_list import image_list
+
 dependsOnUrl = False # True
 extension = '.png'
-target = [
-    # {
-    #     "name": "son-astonished",
-    #     "value": "https://emoji.slack-edge.com/T02R0EL7S9J/son-astonished/3c0aeaba674ef209.png",
-    # }
-    # {
-    #   "name": "부탁할-때-대학일기1",
-    #   "value": "https://item.kakaocdn.net/do/6090dd413202e6645bea7282f80be44e8f324a0b9c48f77dbce3a43bd11ce785"
-    # },
-    {
-      "name": "test",
-      "value": "https://dcimg5.dcinside.com/dccon.php?no=62b5df2be09d3ca567b1c5bc12d46b394aa3b1058c6e4d0ca41648b65ced266e7f14c04a1f84cae4cf517a44b1fb5e5084e411f8fc0a805bc754d93996f95fcbea733749c96b"
-    }
-]
+target = image_list
 
 parentDir = "c:/dev/test/"
 if not os.path.isdir(parentDir):
@@ -25,7 +14,7 @@ if not os.path.isdir(parentDir):
 
 str = ""
 for ele in target:
-    url = ele["value"]
+    url = ele["url"]
     if (dependsOnUrl):
         extension = url[url.rfind("."): len(url)]
     fileName = ele["name"] + extension
@@ -36,7 +25,7 @@ with open(parentDir + "preview.txt", "w") as f:
     f.write(str)
 
 for ele in target:
-    url = ele["value"]
+    url = ele["url"]
     if (dependsOnUrl):
         extension = url[url.rfind("."): len(url)]
     fileName = ele["name"] + extension
---
+++ worker/image-downloader/target_list.py
@@ -0,0 +1,3 @@
+image_list = [
+    {'name':'호락호락하지-않은-니니즈1', 'url':"https://item.kakaocdn.net/dn/W7krk/btszJ14EzUU/BLbaC2efytI4nDx8OqHKz0/img.png"},
+]
